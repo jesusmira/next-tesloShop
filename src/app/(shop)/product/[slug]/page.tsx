30253f2ace5b521136835802aa18a7d8f0455a33
Jesús Mira <jesmira@hotmail.es>
2025-06-11 11:50:43 +0200

    Fin Sección 18 - Producto y Open Graph Information

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,17 +1,49 @@
+export const revalidate = 604800; // 7 dias
+
+import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
-import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
+
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector, StockLabel } from "@/components";
 import { titleFont } from "@/config/fonts";
-import { initialData } from "@/seed/seed";
+import { getProductBySlug } from "@/actions";
 
 interface Props {
   params: Promise<{ slug: string }>
 }
 
 
+ 
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = (await params).slug
+
+  const product = await getProductBySlug(slug);
+ 
+  // fetch post information
+  // const post = await fetch(`https://api.vercel.app/blog/${slug}`).then((res) =>
+  //   res.json()
+  // )
+ 
+  return {
+    title: product?.title ?? 'Producto no encontrado',
+    description: product?.description ?? '',
+    openGraph: {
+      title: product?.title ?? 'Producto no encontrado',
+      description: product?.description ?? '',
+      // images: [], // https://misitioweb.com/products/image.png
+      images: [ `/products/${product?.images[1]}` ],
+      // url: `https://teslo.vercel.app/product/${slug}`,
+    },
+  }
+}
+
+
 export default async function ProductBySlug({ params }: Props) {
 
   const { slug } = await params;
-  const product = initialData.products.find( product => product.slug === slug);
+  const product = await getProductBySlug(slug);
 
   if (!product) notFound();
 
@@ -41,7 +73,11 @@ export default async function ProductBySlug({ params }: Props) {
       { /* Details */}
       <div className="col-span-1 px-5 ">
         
-        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
+       <StockLabel slug={product.slug} />
+
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
 
         <p className="text-lg mb-5">${product.price}</p>
 
